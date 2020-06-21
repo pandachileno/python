ab980046e3db2424cf88870ec734bee4614dff8d
pandachileno <64978421+pandachileno@users.noreply.github.com>
2020-06-21 17:37:49 +0200

    Add files via upload
    Upload Initial Towers Of Hanoi

---
+++ Towers of Hanoi/towersOfHanoi.py
@@ -0,0 +1,84 @@
+class Node:
+  def __init__(self, value, link_node=None):
+    self.value = value
+    self.link_node = link_node
+    
+  def set_next_node(self, link_node):
+    self.link_node = link_node
+    
+  def get_next_node(self):
+    return self.link_node
+  
+  def get_value(self):
+    return self.value
+
+class Stack:
+  def __init__(self, name):
+    self.size = 0
+    self.top_item = None
+    self.limit = 1000
+    self.name = name
+  
+  def push(self, value):
+    if self.has_space():
+      item = Node(value)
+      item.set_next_node(self.top_item)
+      self.top_item = item
+      self.size += 1
+    else:
+      print("No more room!")
+
+  def pop(self):
+    if self.size > 0:
+      item_to_remove = self.top_item
+      self.top_item = item_to_remove.get_next_node()
+      self.size -= 1
+      return item_to_remove.get_value()
+    print("This stack is totally empty.")
+
+  def peek(self):
+    if self.size > 0:
+      return self.top_item.get_value()
+    print("Nothing to see here!")
+
+  def has_space(self):
+    return self.limit > self.size
+
+  def is_empty(self):
+    return self.size == 0
+  
+  def get_size(self):
+    return self.size
+  
+  def get_name(self):
+    return self.name
+  
+  def print_items(self):
+    pointer = self.top_item
+    print_list = []
+    while(pointer):
+      print_list.append(pointer.get_value())
+      pointer = pointer.get_next_node()
+    print_list.reverse()
+    print("{0} Stack: {1}".format(self.get_name(), print_list))
+
+print("\nLet's play Towers of Hanoi!!")
+
+#Create the Stacks
+stacks = []
+left_stack = Stack("Left")
+middle_stack = Stack("Middle")
+right_stack = Stack("Right")
+
+stacks += [left_stack, middle_stack, right_stack]
+
+#Set up the Game
+num_disks = int(input("\nHow many disks do you want to play with?\n"))
+
+while num_disks < 3:
+  num_disks = int(input("Enter a number greater than or equal to 3\n"))
+
+#Get User Input
+
+        
+#Play the Game
